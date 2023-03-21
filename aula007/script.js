0ff4fc37a9e79f6f5a01eedfcd7e1a544dc1e8a0
Glaskario <shvavzhjnsbsjsjsjsj@gmail.com>
2023-03-21 10:30:08 -0300

    aula009

---
+++ aula007/script.js
@@ -0,0 +1,15 @@
+var corpo = document.body
+var p1 = document.getElementsByTagName('p')[0]
+var d = document.querySelector('div#d')
+
+corpo.style.backgroundColor = 'black'
+
+corpo.style.color = 'green'
+
+corpo.style.fontFamily = 'Arial, Helvetica, sans-serif'
+
+document.write(p1.innerHTML)
+
+d.style.background = 'green'
+
+d.style.color = 'white'
--- aula07/script.js
+++
@@ -1,7 +0,0 @@
-window.alert('vamos multiplicar valores!')
-var n1 = window.prompt.('Digite um numero!')
-var n2 = window.prompt.('Digite outro numero!')
-
-var resultado = n1*n2
-document.write("O resultado da conta é:" (resultado) )
-
