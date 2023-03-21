5a5e6d015ab25cf1ca4f1aea976ac0200e7ad1ff
Glaskario <shvavzhjnsbsjsjsjsj@gmail.com>
2023-03-21 09:21:26 -0300

    atividade 007

---
+++ aula07/script.js
@@ -0,0 +1,7 @@
+window.alert('vamos multiplicar valores!')
+var n1 = window.prompt.('Digite um numero!')
+var n2 = window.prompt.('Digite outro numero!')
+
+var resultado = n1*n2
+document.write("O resultado da conta é:" (resultado) )
+
