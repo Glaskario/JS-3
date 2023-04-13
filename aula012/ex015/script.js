9494c0aa5926d6e44eaa45bb127abf9c914ceb28
Glaskario <shvavzhjnsbsjsjsjsj@gmail.com>
2023-04-13 09:46:04 -0300

    aaaaa

---
+++ aula012/ex015/script.js
@@ -0,0 +1,53 @@
+function verificar(){
+
+var data = new Date()
+var ano = data.getFullYear()
+var fano = window.document.getElementById('txtano')
+var res = document.getElementById('res')
+if(fano.value.length == 0  || fano.value > ano){
+    window.alert('Verifique os dados e tente novamente!')
+}else{
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if(fsex[0].checked){
+
+        genero = 'Homem'
+        if(idade >= 0 && idade < 10){
+            //criança
+            img.setAttribute('src', 'imagens/homem_crianca.png')
+        }else if(idade < 21){
+            //jovem
+
+        }else if(idade < 50){
+            //adulto
+
+        }else {
+            //idoso
+        }
+    }else if(fsex[1].checked){
+        genero = 'Mulher'
+        if(idade >= 0 && idade < 10){
+            //criança
+            img.setAttribute('src', 'imagens/mulher_crianca.png')
+        }else if(idade < 21){
+            //jovem
+
+        }else if(idade < 50){
+            //adulto
+
+        }else {
+            //idoso
+        }
+    }
+    
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+}
+
+
+
+}
+res.style.textAlign = 'center'
